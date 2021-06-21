e6e475ebd6f639ca66bb2646ece4154a337c0e8e
Saaurav <prakashsaurav123@gmail.com>
2021-06-21 16:17:31 +0530

    dice init

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() {
+  return runApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.green,
-                backgroundImage: AssetImage('images/android_hilt.png'),
-              ),
-              Text(
-                "Saurav Prakash",
-                style: TextStyle(
-                    fontSize: 32.0,
-                    fontFamily: 'Pacifico',
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text("Android Developer",
-                  style: TextStyle(
-                      fontSize: 22.0,
-                      color: Colors.white,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold)),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.shopping_cart_outlined,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 20.0,
-                      ),
-                      Text(
-                        '+918116364712',
-                        style: TextStyle(fontSize: 20.0, color: Colors.teal),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.email_outlined,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 20.0,
-                      ),
-                      Text(
-                        'prakash123@gmail.com',
-                        style: TextStyle(fontSize: 20.0, color: Colors.teal),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
         ),
+        body: DicePage(),
       ),
-    );
-  }
+    ),
+  );
 }
 
-//test push
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
