5d9f0c4a389a8701481bb522feb24a277348fc40
Saaurav <prakashsaurav123@gmail.com>
2021-06-21 15:54:41 +0530

    card

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,75 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                width: 100.0,
-                color: Colors.green,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.green,
+                backgroundImage: AssetImage('images/android_hilt.png'),
               ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.pink,
+              Text(
+                "Saurav Prakash",
+                style: TextStyle(
+                    fontSize: 32.0,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.deepOrange,
+              Text("Android Developer",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold)),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.shopping_cart_outlined,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        '+918116364712',
+                        style: TextStyle(fontSize: 20.0, color: Colors.teal),
+                      )
+                    ],
+                  ),
+                ),
               ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email_outlined,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'prakash123@gmail.com',
+                        style: TextStyle(fontSize: 20.0, color: Colors.teal),
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
