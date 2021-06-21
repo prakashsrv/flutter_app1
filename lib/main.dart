2f31d35e99b7190f920f358efccd54984c66e98c
Saaurav <prakashsaurav123@gmail.com>
2021-06-21 10:28:12 +0530

    statelessWidgetExample

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-              title: Center(child: Text("I AM RICH")), backgroundColor: Colors.blueGrey),
-          body: Center(child: Image(image: AssetImage('images/android_hilt.png'))),
-        ),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: Center(child: Text("I AM RICH")), backgroundColor: Colors.blueGrey),
+        body: Center(child: Image(image: AssetImage('images/android_hilt.png'))),
       ),
     );
+  }
+}
+
+
 //test push
