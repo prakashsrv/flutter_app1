0a077e43b4cc56f78caa5d9a272be124baf08fb2
Saaurav <prakashsaurav123@gmail.com>
2021-06-22 13:07:15 +0530

    QuizAppChallenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,148 @@
-import 'package:audioplayers/audioplayers.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(Quizzler());
 
-class XylophoneApp extends StatelessWidget {
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Card(
-                color: Colors.red,
-                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                ),
-              ),
-              Card(
-                color: Colors.green,
-                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                ),
-              ),
-              Card(
-                color: Colors.blue,
-                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                ),
-              ),
-              Card(
-                color: Colors.yellow,
-                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                ),
-              ),
-              Card(
-                color: Colors.pink,
-                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                ),
-              ),
-              Card(
-                color: Colors.deepOrange,
-                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                ),
-              ),
-              Card(
-                color: Colors.lightBlueAccent,
-                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                ),
-              ),
-              GestureDetector(
-                onTap:(){
-
-              },
-                child: Card(
-                  color: Colors.teal,
-                  margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-                  child: Padding(
-                    padding: EdgeInsets.all(50.0),
-                  ),
-                ),
-              ),
-            ],
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
         ),
       ),
     );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+  ];
+
+  List<String> questions = [
+    "India is a country?",
+    "India is a state?",
+    "India is a city?",
+    "India is a place?"
+  ];
+  static var _random = new Random();
+  static var questionNumber = _random.nextInt(2) + 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions.elementAt(questionNumber),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {});
+                if (questionNumber == 1) {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.check_outlined,
+                      color: Colors.red,
+                    ),
+                  );
+                  questionNumber = _random.nextInt(2) + 1;
+                  setState(() {
+
+                  });
+                } else if (questionNumber == 2) {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.green,
+                    ),
+                  );
+                  questionNumber = _random.nextInt(2) + 1;
+                }else if(questionNumber == 3){
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.green,
+                    ),
+                  );
+                  questionNumber = _random.nextInt(2) + 1;
+                } else {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.check_outlined,
+                      color: Colors.red,
+                    ),
+                  );
+                  questionNumber = _random.nextInt(2) + 1;
+                }
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        Expanded(child: Row(children: scoreKeeper))
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
