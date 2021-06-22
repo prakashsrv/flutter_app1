19c4c0c831d3042a2da1320081d080df5967e705
Saaurav <prakashsaurav123@gmail.com>
2021-06-22 10:47:02 +0530

    xylo

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,83 @@
-import 'dart:math';
-
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Card(
+                color: Colors.red,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                child: Padding(
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ),
+              Card(
+                color: Colors.green,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                child: Padding(
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ),
+              Card(
+                color: Colors.blue,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                child: Padding(
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ),
+              Card(
+                color: Colors.yellow,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                child: Padding(
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ),
+              Card(
+                color: Colors.pink,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                child: Padding(
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ),
+              Card(
+                color: Colors.deepOrange,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                child: Padding(
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ),
+              Card(
+                color: Colors.lightBlueAccent,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                child: Padding(
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ),
+              GestureDetector(
+                onTap:(){
+
+              },
+                child: Card(
+                  color: Colors.teal,
+                  margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(50.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        body: DicePage(),
-      ),
-    ),
-  );
-}
-
-/*class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(child: TextButton(
-            onPressed: (){
-              print("ON PRESSED");
-            },
-            child: Image(
-              image: AssetImage('images/dice1.png'),
-            ),
-          )),
-          Expanded(child: Image(
-            image: AssetImage('images/dice1.png'),
-          ))
-        ],
-      ),
-    );
-  }
-}*/
-
-class DicePage extends StatefulWidget {
-  const DicePage({Key? key}) : super(key: key);
-
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDice=1;
-  var _diceface=1;
-  var _diceface2=1;
-
-  void changeDice(){
-    _diceface = Random().nextInt(5) +1 ;
-    _diceface2 = Random().nextInt(5) +1 ;
-  }
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(child: TextButton(
-            onPressed: () {
-              setState(() {
-                changeDice();
-              });
-              print("ON PRESSED");
-            },
-            child: Image(
-              image: AssetImage('images/ball$_diceface.png'),
-            ),
-          )),
-          Expanded(child: TextButton(
-            onPressed: (){
-              setState(() {
-                changeDice();
-              });
-            },
-            child: Image(
-              image: AssetImage('images/ball$_diceface2.png'),
-            ),
-          ))
-        ],
       ),
     );
   }
 }
-
-
